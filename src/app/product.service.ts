26013173c506cff025ade346a08afbc683cdd9b8
gaikwadk <gaikwadk@vmware.com>
2018-07-27 10:39:30 +0530

    jkbdjcbd

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,7 +3,8 @@ import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map'; 
 
-
+import {Album} from './album';
+import { Observable } from '../../node_modules/rxjs/Observable';
 //import { timingSafeEqual } from 'crypto';
 
 
@@ -13,8 +14,8 @@ export class ProductService {
   private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) {}
 
-  getAlbum(id : number){
-    return this._http.get(this._albumUrl).map((response)=>response.json());
+  getAlbum(id : number): Observable<Album>{
+    return this._http.get(this._albumUrl).map((response)=><Album>response.json());
   }
 
 }
